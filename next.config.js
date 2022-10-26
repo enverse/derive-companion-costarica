66b3572bca8e1f4634c529137a199253a7e0f872
Chris <chrislhamelin@gmail.com>
2022-10-26 12:55:41 +0200

    add multi env docker config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextTranslate = require('next-translate');
 const nextConfig = nextTranslate({
   reactStrictMode: true,
   swcMinify: true,
+  output: 'standalone',
 });
 
 module.exports = nextConfig;
